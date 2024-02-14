670e0e06754b7e25b7f73c83570ee9567f7aaaf4
alifprihantoro <alifprihantoro@gmail.com>
2024-02-14 14:55:28 +0700

    change configs replace link

--- lua/muryp-link/configs.lua
+++ lua/muryp-link/configs.lua
@@ -3,7 +3,15 @@ local DEFAULT_CONFIGS = {
   ---@param TEXT string
   ---@return string
   replaceTexttoLink = function(TEXT)
-    return '[' .. TEXT .. '](' .. TEXT .. ')'
+    -- Remove symbols
+    local LINK = TEXT:gsub('[%p%c]', '')
+    -- remove emoji
+    LINK = LINK:gsub('[\128-\255][\128-\191]*', '')
+    -- replace space
+    LINK = LINK:gsub('%s+', '-')
+    -- Convert to lowercase
+    LINK = LINK:lower()
+    return '[' .. TEXT .. '](' .. LINK .. ')'
   end,
   ---if you use `/slug` maybe you cannot open the file. with this you can direct slug into the file
   ---@param FILE string link non url (like file/slug/not contains http(s))
